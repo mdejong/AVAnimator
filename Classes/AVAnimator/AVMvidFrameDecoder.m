c79dd1b0541315f3e78bf05e56e4203ba37d074b
Mo DeJong <mo@modejong.com>
2015-10-24 20:19:52 -0700

    verify that .mvid file version is at least 2 in _openAndCopyHeaders so that it will not be possible to open an old version 0 or version 1 .mvid file even in opt mode and if assert is disabled in project settings

--- Classes/AVAnimator/AVMvidFrameDecoder.m
+++ Classes/AVAnimator/AVMvidFrameDecoder.m
@@ -286,7 +286,23 @@
     
     if (worked) {
       uint32_t bpp = hPtr->bpp;
-      NSAssert(bpp == 16 || bpp == 24 || bpp == 32, @"bpp must be 16, 24, 32");
+      if (bpp == 16 || bpp == 24 || bpp == 32) {
+        // nop
+      } else {
+        worked = FALSE;
+        NSAssert(FALSE, @"bpp must be 16, 24, 32");
+      }
+    }
+
+    if (worked) {
+      // Verify the minimum file version, previously version 0 and 1 files
+      // were acceptable but version 2 is now required to support ARM64.
+      // The only exception is the upgrade path where older versions are allowed.
+      
+      if (self.upgradeFromV1 == FALSE && maxvid_file_version(hPtr) < MV_FILE_VERSION_TWO) {
+        worked = FALSE;
+        NSAssert(FALSE, @"only .mvid files version 2 or newer can be used, you must -upgrade this .mvid from version %d", maxvid_file_version(hPtr));
+      }
     }
   }
   
@@ -515,17 +531,6 @@
   if (newFrameIndex >= numFrames) {
     NSAssert(FALSE, @"%@: %d", @"can't advance past last frame", (int) newFrameIndex);
   }
-  
-#if defined(EXTRA_CHECKS)
-  // Verify that input file is mvid version 2. Version 0 and 1 files will not
-  // load properly on ARM64 hardware.
-  
-  MVFileHeader *header = [self header];
-  
-  if (self.upgradeFromV1 == FALSE && maxvid_file_version(header) < MV_FILE_VERSION_TWO) {
-    NSAssert(FALSE, @"only .mvid files version 2 or newer can be used, you must -upgrade this .mvid from version %d", maxvid_file_version(header));
-  }
-#endif // EXTRA_CHECKS
   
   BOOL changeFrameData = FALSE;
   const int newFrameIndexSigned = (int) newFrameIndex;
